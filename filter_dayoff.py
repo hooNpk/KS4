ff3e5e7f5137ec307cc7de08fe870f800db76b54
hooN <johnhpark11@gmail.com>
2020-01-08 06:27:37 +0900

    complete importing holiday from open API, managing weekend

--- filter_dayoff.py
+++ filter_dayoff.py
@@ -3,29 +3,30 @@
 #인증키 :VbQNuye6GhNGAQ00jwTa6eGyuFnBY%2B9bmGMYEVXsqBY94ARYroVyHA0WxwOse6em%2FdhcrPZUWBvsoCgl6sDrww%3D%3D
 
 from urllib.parse import urlencode, quote_plus
-from urllib.request import Request, urlopen
-import numpy as np
-import xml.etree.ElementTree as ET
+import requests
+import pandas as pd
+import bs4 as BeautifulSoup
 
 url = 'http://apis.data.go.kr/B090041/openapi/service/SpcdeInfoService/getRestDeInfo?'
 def return_dayoff_list(year, month):
 
-  dayoff = np.empty(5)
+  dayoff = []
   
   queryParams = 'solYear={year_r}&solMonth={month_r}&ServiceKey=VbQNuye6GhNGAQ00jwTa6eGyuFnBY%2B9bmGMYEVXsqBY94ARYroVyHA0WxwOse6em%2FdhcrPZUWBvsoCgl6sDrww%3D%3D'.format(year_r=year, month_r=month)
-  request = Request(url+queryParams)
-  request.get_method = lambda:'GET'
-  response_body = urlopen(request).read()
-  print(response_body)
-  tree = ET.fromstring(response_body)
-  
-  for date in tree.findall('locdate'):
-    print(date)
-    print(date.text)
-  
+  r = requests.get(url+queryParams)
+  body = r.text
+  soup = BeautifulSoup.BeautifulSoup(body, features='lxml')
+  soup.prettify()
+
+  for date in soup.find_all('locdate'):
+    dayoff.append(pd.to_datetime(date.text))
+  return dayoff
+
+def is_weekend(date):
+
 
 def main():
-  return_dayoff_list('2020', '05')
+  a = return_dayoff_list('2020', '04')
 
 if __name__ == "__main__":
   main()
